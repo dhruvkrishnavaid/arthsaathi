2e3614fd4be23d58488f5c94d65800c48b04f057
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2025-01-12 17:31:38 +0530

    append home & learning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,9 @@ import BusinessCard from '../components/BusinessCard';
 
 function Home() {
   return (
-    <div className='pl-2 pt-2'>
+    <div className='p-2 overflow-x-hidden'> 
     <div className='flex'>
-    <div>
+    <div className='w-[20%]'>
     <div className="text-xl font-semibold">
       Hey Y'all!
     </div>
@@ -13,17 +13,17 @@ function Home() {
     Welcome to arthsaathi...
     </div>
     </div>
-      <img src={HomeGuy} alt="HomeGuy" className='w-[555px] h-[555px] ml-auto mr-10'/>
+      <img src={HomeGuy} alt="HomeGuy" className='w-[300px] ml-[500px] scale-152'/>
     </div>
 
     <div className='text-xl font-semibold mt-5'>
     Explore Businesses
     </div>
     <div className='flex'>
-    <div>
+    <div className='mr-5 mt-5'>
       <BusinessCard />
     </div>
-    <div>
+    <div className='mt-5'>
       <BusinessCard />
     </div>
     </div>
